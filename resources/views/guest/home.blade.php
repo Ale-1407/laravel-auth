2f2650f770d168404bd2c787d2bb912cec8a85af
Ale-1407 <alessiacarrera07@gmail.com>
2023-01-25 10:11:11 +0100

    modifica struttura vue con component

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Front Office</title>
-    {{-- style --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-</head>
-<body>
-    {{-- Vue --}}
+@extends('layouts.guestLayout')
+@section('content')
     <div id="root">
 
     </div>
-
-    {{-- file js --}}
-    <script src="{{ asset('js/app.js')}}"></script>
-</body>
-</html>
+@endsection
